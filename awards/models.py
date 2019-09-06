7786d1cd5286399dc62a7b95878b8e3f46a3db58
markmumba <markmumba01@gmail.com>
2019-09-06 13:16:02 +0300

    finished the class for the profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profile/',null = True)
+    user_bio =models.TextField()
+    user = models.ForeignKey(User ,null=True)
+
+
+class Project(models.Model):
+    photo = models.ImageField(upload_to= 'images/' , null = True)
+    link = models.TextField()
