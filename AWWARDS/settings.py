b5ff3a378593941bdc6808e3675479d068182b4c
markmumba <markmumba01@gmail.com>
2019-09-09 11:12:01 +0300

    deployment to heroku

--- AWWARDS/settings.py
+++ AWWARDS/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from decouple import config
 import django_heroku
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,7 +95,7 @@ DATABASES = {
         'PASSWORD':config('DB_HOST')
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
+
 DATABASES['default'].update(db_from_env)
 
 
